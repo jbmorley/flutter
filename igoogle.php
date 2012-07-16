070964992329d5056fe94989a4b38a724f7f8e12
inertia <inertia@42999651-55c4-4f38-b74b-1d7a0563c3cd>
2012-07-16 08:22:26 +0000

    Getting the latest working version into mercurial.

---
+++ igoogle.php
@@ -0,0 +1,32 @@
+<?php
+	header ("Content-Type:text/xml");
+	print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+?>
+<Module>
+<ModulePrefs title="Flutter" height="350" />
+<Content type="html"><![CDATA[
+
+	<style>
+		body
+			{
+			text-align: center;
+			}
+	
+		#flutter
+			{
+			border: 0;
+			margin: 0;
+			padding: 0;
+			overflow: hidden;
+			margin: auto;
+			width: 100%;
+			height: 100%;
+			}
+	</style>
+		
+	<body>
+		<iframe id="flutter" src="http://www.jbmorley.co.uk/flit/flit.php"></iframe>
+	</body>
+
+]]></Content>
+</Module>
