b7265451bb90b8016b50aecd9424a48a301fa38f
inertia <inertia@42999651-55c4-4f38-b74b-1d7a0563c3cd>
2012-07-16 08:23:49 +0000

    Adding Google analytics to the script.

--- flutter.php
+++ flutter.php
@@ -38,7 +38,17 @@
 		<div id='all'>
 			<ul id="feed"></ul>
 		</div>
-		
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8809677-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
 	</body>
 	
 </html>
